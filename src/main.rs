a40ea1469c5b52b836887117d15168d98ab73891
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-05-23 01:27:05 +0100

    rewrite with fli

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,62 @@
-use std::{env, net::{IpAddr, TcpStream, SocketAddr}, str::FromStr, process, sync::mpsc::{Sender, channel}, thread, io::{self, Write}};
+extern crate fli;
+use fli::Fli;
+use std::{ net::{IpAddr, TcpStream}, str::FromStr, process, sync::mpsc::{Sender, channel}, thread, io::{self, Write}};
 
 
 const MAX_PORT : u16 = 65535;
 struct Arguments{
-    flag :String,
     ipaddr:IpAddr,
-    threads: u16
+    threads: u16,
 }
 
 impl Arguments {
-    fn new(args: &[String]) -> Result<Arguments, &'static str> 
+    fn new(addr: String, threads : String) -> Result<Arguments, &'static str>
     {
-        if args.len() < 2 {
-            return Err("Not enough argument");
-        }
-        if args.len() > 4 {
-            return Err("Too many arguments");
-        }
-        let mut flag =  args[1].clone();
-        if let Ok(ipaddr) = IpAddr::from_str(&flag) {
-            return Ok(Arguments{flag : String::from(""), ipaddr, threads: 4});
-        }
-        else{
-            flag = args[1].clone();
-            if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
-                println!("Help statement");
-                return Err("help");
-            }
-            if flag.contains("-j") {
-                let ipaddr = match IpAddr::from_str(&args[3]) {
-                    Ok(s) => s,
-                    Err(_) =>  return Err("Invalid IpAddress"),
-                };
-                let threads = match args[2].parse::<u16>() {
-                    Ok(thread) => thread,
-                    Err(_) => return Err("Invalid thread")
-                };
-                return Ok(Arguments{flag, ipaddr, threads});
-            }
-        }
-        return Err("An Error Occcured : Failed to init struct")
+        let ipaddress = match IpAddr::from_str(&addr) {
+            Ok(s) => s,
+            Err(_) =>  return Err("Invalid IpAddress"),
+        };
+        let threads = match threads.parse::<u16>() {
+            Ok(thread) => thread,
+            Err(_) => return Err("Invalid thread")
+        };
+        return Ok(Arguments{ipaddr : ipaddress, threads});
     }
 }
 
 
 fn main() {
-   let args : Vec<String> = env::args().collect();
-    println!("Hello, world!, {:?}", args);
-    run_callback(make);
-    let arguments = match Arguments::new(&args) {
-        Ok(arg) => arg,
-        Err(err) => {
-            println!("{}", err);
-            if err.contains("help") {
+    let mut app = Fli::init("sniffer", "an app to sniff port");
+    app.command("snif", "snif default");
+    app.option("-ip --ipaddress, <>","to set ipaddress", snif);
+    app.option("-th --thread, []", "to set thread number default 4", |_x|{});
+    app.run();
+}
 
-                process::exit(0)
-            }
-            process::exit(1)
+fn snif(x: &Fli)
+{
+    let ipaddress = match x.get_values("--ipaddress".to_owned()){
+        Ok(values) =>  values[0].to_string(),
+        Err(_) => "".to_string(),
+    };
+    let thread_num = match x.get_values("--thread".to_owned()){
+            Ok(values) => values[0].to_string(),
+            Err(_) => "4".to_string()
+    };
+    let arguments = match Arguments::new(ipaddress, thread_num) {
+        Ok(arg) =>  arg,
+        Err(e) => {
+            println!("{e}");
+            process::exit(1);
         }
     };
-
-    let num_thread = arguments.threads;
-    let addr = arguments.ipaddr;
+    let thread_num = arguments.threads;
+    let ipaddress = arguments.ipaddr;
     let (tx, rx) = channel();
-    for i in 0..num_thread {
+    for i in 0..thread_num {
         let tx = tx.clone();
         thread::spawn(move || {
-            print!("Running \n");
-            scan(tx, i, addr , num_thread);
+            scan(tx, i, ipaddress , thread_num);
         });
     }
     let mut out = vec![];
@@ -78,17 +67,17 @@ fn main() {
     println!("");
     out.sort();
     for v in out{
-        println!("{} is opne ", v);
+        println!("{} is open ", v);
     }
 }
 
+
 fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr,  num_thread : u16) {
     let mut port = start_port + 1;
-    println!("No of thread {}", num_thread);
     loop {
         match TcpStream::connect((addr, port)) {
             Ok(_) => {
-                println!(". Port : {}", port);
+                println!(".");
                 io::stdout().flush().unwrap();
                 tx.send(port).unwrap();
             },
@@ -100,13 +89,5 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr,  num_thread : u16) {
             break;
         }
         port += num_thread;
-        println!("Port+thread = {}", port);
     }
-}
-
-
-
-fn run_callback(callback : fn())
-{
-    callback();
 }
